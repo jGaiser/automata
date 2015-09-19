7d0fa8529af93f7d333774657981cc8b1dea6b52
Jeremiah Gaiser <jeraiser@gmail.com>
2015-09-19 13:34:39 -0600

    add mousemove event handler

--- automata.js
+++ automata.js
@@ -3,6 +3,8 @@ var automata = (function(){
       ctx = canvas.getContext('2d'),
       cellSize = 50,     
       cellArray = [],
+      mouse = {x:0, y:0},
+      //iterators
       i,
       n;
 
@@ -10,15 +12,24 @@ var automata = (function(){
     this.x = x;
     this.y = y; 
     this.w = w;
+    this.power = 0.7;
     this.color = 'rgb(30,30,30)';
+    this.fillColor = 'rgba(50, 100, 150, ' + this.power + ')'; 
+    console.log(this.fillColor);
 
     this.draw = function(){
       ctx.save();
       ctx.lineWidth = 1; 
       ctx.strokeStyle = this.color;
+      ctx.fillStyle = this.fillColor;
       ctx.beginPath();
       ctx.rect(this.x, this.y, w, w);
       ctx.stroke();
+
+      if(this.power > 0){
+        ctx.fillRect(this.x, this.y, w, w);
+      }
+    
       ctx.restore();    
     }
 
@@ -46,6 +57,14 @@ var automata = (function(){
     canvas.setAttribute('width', window.innerWidth);
     canvas.setAttribute('height', window.innerHeight);  
   }
+
+  function addEventListeners(){
+    document.onmousemove = function(e){
+      mouse.x = e.pageX;
+      mouse.y = e.pageY; 
+      console.log(mouse);
+    }  
+  }
  
   function testIt(){
     buildGrid();
@@ -53,6 +72,7 @@ var automata = (function(){
     
   function init(){
     resizeCanvas();  
+    addEventListeners;
     testIt();
   }
 
