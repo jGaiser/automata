fda0862f1308744bc9d0bc36f65a4b7b72d5777d
Jeremiah Gaiser <jeraiser@gmail.com>
2015-09-18 19:00:48 -0600

    write cell class

--- automata.js
+++ automata.js
@@ -6,21 +6,35 @@ var automata = (function(){
     this.x = x;
     this.y = y; 
     this.w = w;
+    this.color = 'rgb(30,30,30)';
 
     this.draw = function(){
-        
+      ctx.save();
+      
+      ctx.strokeStyle = this.color;
+      ctx.beginPath();
+      ctx.rect(this.x, this.y, w, w);
+      ctx.stroke();
+      ctx.restore();    
     }
   }
-
+  
   function resizeCanvas(){
     canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerWidth);  
+    canvas.setAttribute('height', window.innerHeight);  
   }
-      
-  
+ 
+  function testIt(){
+    var newCell = new Cell(50, 50, 50);
+    newCell.draw();  
+    console.log(newCell);
+  } 
     
   function init(){
     resizeCanvas();  
+    testIt();
+    var newCell = new Cell();
+    newCell.draw();  
   }
 
   init();
