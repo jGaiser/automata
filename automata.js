8f7ac07785e8c87fd3c6ee8cfde80602b3921c53
Jeremiah Gaiser <jeraiser@gmail.com>
2015-09-20 15:25:08 -0600

    Add hover event and some simple rules

--- automata.js
+++ automata.js
@@ -1,9 +1,15 @@
 var automata = (function(){
   var canvas = document.getElementById('viewport'),
       ctx = canvas.getContext('2d'),
+      canvasWidth,
+      canvasHeight,
       cellSize = 50,     
       cellArray = [],
+      activeCells = [],
       mouse = {x:0, y:0},
+      cellWidth,
+      gridWidth,
+      gridHeight,
       //iterators
       i,
       n;
@@ -12,10 +18,9 @@ var automata = (function(){
     this.x = x;
     this.y = y; 
     this.w = w;
-    this.power = 0.7;
+    this.power = 0.0;
     this.color = 'rgb(30,30,30)';
     this.fillColor = 'rgba(50, 100, 150, ' + this.power + ')'; 
-    console.log(this.fillColor);
 
     this.draw = function(){
       ctx.save();
@@ -24,7 +29,7 @@ var automata = (function(){
       ctx.fillStyle = this.fillColor;
       ctx.beginPath();
       ctx.rect(this.x, this.y, w, w);
-      ctx.stroke();
+//      ctx.stroke();
 
       if(this.power > 0){
         ctx.fillRect(this.x, this.y, w, w);
@@ -33,13 +38,31 @@ var automata = (function(){
       ctx.restore();    
     }
 
+    this.hoverHandler = function(){
+      this.power = 1;  
+      this.fillColor = 'rgba(50, 100, 150, ' + this.power + ')'; 
+      
+      if(activeCells.indexOf(this) == -1){
+        activeCells.push(this);
+      }
+    }
+
+    this.step = function(){
+      this.power -= 0.10;   
+      this.fillColor = 'rgba(50, 100, 150, ' + this.power + ')'; 
+
+      if(this.power < 0){
+        activeCells.splice(activeCells.indexOf(this), 1);   
+      }
+    }
+
     cellArray.push(this);
   }
 
   function buildGrid(){
-    var cellWidth = canvas.getAttribute('width') / cellSize, 
-        gridWidth = canvas.getAttribute('width') / cellWidth,
-        gridHeight = canvas.getAttribute('height') / cellWidth; 
+    cellWidth = cellWidth   || canvas.getAttribute('width') / cellSize; 
+    gridWidth = gridWidth   || canvas.getAttribute('width') / cellWidth;
+    gridHeight = gridHeight || canvas.getAttribute('height') / cellWidth; 
     
     for(i = 0; i <= gridWidth; i++){
       for(n = 0; n <= gridHeight; n++){ 
@@ -50,19 +73,45 @@ var automata = (function(){
     for(i = 0; i < cellArray.length; i++){
       cellArray[i].draw(); 
     } 
-    console.log(cellArray);
   }
   
   function resizeCanvas(){
-    canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerHeight);  
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+    canvas.setAttribute('width', canvasWidth);
+    canvas.setAttribute('height', canvasHeight);  
+  }
+
+  
+  function mouseMoveHandler(e){
+    mouse.x = e.pageX - canvas.offsetLeft;
+    mouse.y = e.pageY - canvas.offsetTop;
+    for(i = 0; i < cellArray.length; i++){
+      if(cellArray[i].x < mouse.x && cellArray[i].x + cellWidth > mouse.x){
+        if(cellArray[i].y < mouse.y && cellArray[i].y + cellWidth > mouse.y){
+          cellArray[i].hoverHandler();  
+        }  
+      } 
+    }
+  } 
+  
+  function step(){
+    ctx.clearRect(0,0, canvasWidth, canvasHeight);  
+    for(i = 0; i < cellArray.length; i++){
+      cellArray[i].draw(); 
+    }
+    
+    for(n=0; n < activeCells.length; n++){
+      activeCells[n].step();
+    }
+    
+    console.log(activeCells);
+    requestAnimationFrame(step);
   }
 
   function addEventListeners(){
     document.onmousemove = function(e){
-      mouse.x = e.pageX;
-      mouse.y = e.pageY; 
-      console.log(mouse);
+      mouseMoveHandler(e);
     }  
   }
  
@@ -72,8 +121,9 @@ var automata = (function(){
     
   function init(){
     resizeCanvas();  
-    addEventListeners;
+    addEventListeners();
     testIt();
+    window.requestAnimationFrame(step);
   }
 
   init();
